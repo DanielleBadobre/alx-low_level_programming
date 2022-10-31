1481673cb6bc6aa4abaf45e303c842bf4e016b8e
DanielleBadobre <109845369+DanielleBadobre@users.noreply.github.com>
2022-10-31 23:59:48 +0000

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strchr - locates character in string
+ * strchr - locates character in string
  * @s: string to locate char
  * @c: character to find
  * Return: character value
