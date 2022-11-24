f664756925fe446b46fb6d4fea7f7b0760e20797
Danielle Badobre <badobredanielle@gmail.com>
2022-11-24 01:57:46 +0000

    yeah

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char f,m,l;
+	int age;
+	printf("enter your name and initials with no space in between please: ");
+	scanf("%c %c %c %d", &f, &m, &l, &age);
+	printf("My initials are: %c%c%c and my age is %d.\n",f,m,l,age);
+	return (0);
+}
