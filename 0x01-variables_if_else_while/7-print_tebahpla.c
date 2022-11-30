34ca40fe9800e45455cecc34fc861b29f8e45bcc
Danielle Badobre <badobredanielle@gmail.com>
2022-11-30 14:35:57 +0000

    prints the alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		putchar(c);
+		--c;
+	}
+	putchar('\n');
+	return (0);
+}
