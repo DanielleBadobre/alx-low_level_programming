3310c8bd6478b8f6dd2e77a30f7203137865ce5b
Danielle Badobre <badobredanielle@gmail.com>
2023-07-25 21:52:30 +0000

    fix task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,33 +11,31 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, u, j, v, w, taille;
+	unsigned int i, size1, size2, size;
 	char *nconcat;
 
+	i = 0;
+	size1 = 0;
+	size2 = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	i = 0;
-	while (s1[i] != '\0')
-		i++;
-	j = 0;
-	while (s2[j] != '\0')
-		j++;
-	if (n >= j)
-		taille = j;
+	while (s1[size1])
+		size1++;
+	while (s2[size2])
+		size2++;
+	if (n >= size2)
+		size = size2;
 	else
-		taille = n;
-	nconcat = malloc((i + taille + 1) * sizeof(char));
+		size = n;
+	nconcat = malloc((size1 + size + 1) * sizeof(char));
 	if (nconcat == NULL)
 		return (NULL);
-	for (u = 0; u < i; u++)
-		nconcat[u] = s1[u];
-	v = 0;
-	while (s2[v] != '\0')
-		v++;
-	for (w = i; w < (taille + i); w++)
-		nconcat[w] = s2[w - taille];
+	for (i = 0; i < size1; i++)
+		nconcat[i] = s1[i];
+	for (i = size1; i < (size1 + size); i++)
+		nconcat[i] = *(s2 + i - size1);
+	*(nconcat + i) = '\0';
 	return (nconcat);
-	free(nconcat);
 }
