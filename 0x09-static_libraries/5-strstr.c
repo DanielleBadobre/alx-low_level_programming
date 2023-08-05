216be70606c87b0080435360036b4731afa41ea5
Danielle Badobre <badobredanielle@gmail.com>
2023-08-05 14:03:17 +0000

    redid lib

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,29 +1,43 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * _strpbrk - prints the consecutive caracters of s1 that are in s2.
- * @s: source string
- * @accept: searching string
+ * _strstr - prints the consecutive caracters of s1 that are in s2.
+ * @haystack: source string
+ * @needle: searching string
  *
  * Return: new string.
  */
-char *_strpbrk(char *s, char *accept)
+char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i, j;
-
-	for (i = 0; *(s + i); i++)
+	while (*haystack)
 	{
-		for (j = 0; *(accept + j); j++)
+		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
 		{
-			if (*(s + i) == *(accept + j))
-			{
-				break;
-			}
+			return (haystack);
 		}
-		if (*(accept + j) != '\0')
+		else
 		{
-			return (s + i);
+			haystack++;
 		}
 	}
 	return (0);
+}
+/**
+ * coincidence - define if the string b is inside a.
+ * @a: source string
+ * @b: string to be searched
+ *
+ * Return: 1 if there is coincidence, otherwise 0.
+ */
+int coincidence(char *a, char *b)
+{
+	while (*b && *b == *a)
+	{
+		b++;
+		a++;
+	}
+
+	if (*b == '\0')
+		return (1);
+	else
+		return (0);
 }
