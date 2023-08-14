f62de3ba104063fb070ee1f39c98e9ea74412669
Danielle Badobre <badobredanielle@gmail.com>
2023-08-14 22:54:58 +0000

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 int main(void)
 {
-	printf("%s\n", __BASE_FILE__);
+	char *name = __BASE_FILE__;
+	int i;
+
+	for (i = 0; name[i] != '0'; i++)
+		_putchar(name[i]);
+	_putchar('\n');
 	return (0);
 }
--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -1,2 +1,5 @@
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+#endif
 
