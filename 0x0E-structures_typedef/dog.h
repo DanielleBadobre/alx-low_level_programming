906d45dfcf9aab59b3ba4e0100a5b86dd58b3952
Danielle Badobre <badobredanielle@gmail.com>
2023-08-15 02:10:05 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - describes dog
+ * @name: his name
+ * @age: his age
+ * @owner: his owner
+ * Description: just a dog bruh
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
