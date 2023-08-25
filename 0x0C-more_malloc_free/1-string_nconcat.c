734aa600debf23660e5a7ab4bcd63b688c0b2302
Danielle Badobre <badobredanielle@gmail.com>
2023-08-25 13:02:11 +0000

    checked if s2 is NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s1 = "";
+		s2 = "";
 	for (len1 = 0; s1[len1]; len1++)
 	{}
 	for (len2 = 0; s2[len2]; len2++)
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (count1 = 0; count1 < len1; count1++)
 		concat[count1] = s1[count1];
 	for (count2 = 0; count2 < n && s2[count2]; count1++, count2++)
-			concat[count1] = s2[count2];
+		concat[count1] = s2[count2];
 	concat[count1] = '\0';
 	return (concat);
 }
