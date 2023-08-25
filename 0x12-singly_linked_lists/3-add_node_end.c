3b0df1ac371a74e4cf42bed97cddecfc34e5182d
Danielle Badobre <badobredanielle@gmail.com>
2023-08-25 02:34:34 +0000

    ;beti

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -22,12 +22,14 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
 	list_t *last = *head;
-	
+
 	if (head == NULL)
 		return (NULL);
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->str = strdup(str);
 	if (str == NULL)
 	{
@@ -36,13 +38,16 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
+
 	while (last->next != NULL)
 		last = last->next;
+
 	last->next = new_node;
 	return (new_node);
 
