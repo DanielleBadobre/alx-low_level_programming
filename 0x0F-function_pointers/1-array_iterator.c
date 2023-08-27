e0d7864422a133d07a259a45654978998c77c044
Danielle Badobre <badobredanielle@gmail.com>
2023-08-27 22:09:53 +0000

    2nd task fixed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned long int i;
 
 	if (array != NULL && action != NULL)
-		for (i = 0; i <= (size - 1); i++)
-			action(array[i]);
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
