bdd94de9851a6b3c3fe673f98a65f73dfd1b1d6e
Danielle Badobre <badobredanielle@gmail.com>
2023-08-27 21:42:38 +0000

    advanced task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - return min numb of coins
+ * @argc: count
+ * @argv: vector
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char **argv)
+{
+	int coins, i, amount;
+	int value[5] = {25, 10, 5, 2, 1};
+
+	coins = 0;
+	amount = atoi(argv[argc - 1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (amount <= 0)
+		printf("0\n");
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (value[i] <= amount)
+			{
+				coins = coins + (amount / value[i]);
+				amount = amount - (amount / value[i]) * value[i];
+				if (amount == 0)
+				{
+					printf("%d\n", coins);
+					break;
+				}
+			}
+		}
+	}
+	return (0);
+}
