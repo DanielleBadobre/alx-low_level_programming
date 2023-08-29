1fc2b227a38ddaa17f3ead35f8ecbc0b1500f6e6
Danielle Badobre <badobredanielle@gmail.com>
2023-08-29 22:25:37 +0000

    task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,20 @@
-#include ""
+#include "lists.h"
+/**
+ * sum_listint - returns the sum of all data
+ * @head: head node
+ * Return: sum or 0
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *temp = head;
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	while (temp)
+	{
+		sum = sum + temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
