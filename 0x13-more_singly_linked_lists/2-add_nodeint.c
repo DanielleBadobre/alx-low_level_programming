7bffd06a946fbf9d7096a8f1cbeebd70a0496fb8
Danielle Badobre <badobredanielle@gmail.com>
2023-08-29 21:27:32 +0000

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,5 +7,15 @@
  */
 listnt_t *add_nodeint(listnt_t **head, const int n)
 {
-	
+	listint_t *new_node;
+
+	if (!head)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
