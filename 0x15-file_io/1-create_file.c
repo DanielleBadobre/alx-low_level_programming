0b76543b77ead4ea38320a4cc26461926e89825e
Danielle Badobre <badobredanielle@gmail.com>
2023-09-21 08:05:43 +0000

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * create_file - creates file
+ * @filename: file to read
+ * @text_content: content of file
+ * Return: 2 or 0
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, y;
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	for (y = 0; text_content[i] != '\0'; i++)
+		;
+	write(file, text_content, i);
+	close(file);
+	return (1);
+}
