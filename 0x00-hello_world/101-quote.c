81001dba5a609ad151dc70879af05833a07be106
Danielle Badobre <badobredanielle@gmail.com>
2023-09-27 20:31:51 +0000

    first c project tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints sentence to fwrite
+ * Return: 1
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+	return (1);
+}
